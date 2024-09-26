d8817b5862aacfbd2d0a287bd9389a1c9579e2f9
Ashraf2k4 <117174597+Ashraf2k4@users.noreply.github.com>
2024-09-26 20:19:10 +0530

    Create Color.kt

---
+++ app/src/main/java/com/progress/photos/pixahub/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.progress.photos.pixahub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val green80 = Color(0xFF1FC57A)
+val greenGray80 = Color(0x90AEE0C9)
+val DarkGreen = Color(0xFF156F33)
